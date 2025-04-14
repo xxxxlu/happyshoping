08c0b4ba1836b844a622fb0183b062bef87038fd
xxxlu <xlu17795781004@163.com>
2025-04-14 16:53:25 +0800

    feat: copyright

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@
             </div>
           </div>
         </div>
-        <p>Copyright © 2025 | Powered by Data Network Solutions (SMC-Private) Limited</p>
+        <p>Copyright © 2025 happyshopping Store. Powered by VertexFlow SMC Technologies Pvt Ltd</p>
       </div>
     </footer>
   </div>
